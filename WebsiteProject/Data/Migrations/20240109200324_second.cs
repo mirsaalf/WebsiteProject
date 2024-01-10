138734a2d67ca6931e284eee055fa83faef35c3c
mirsaalf <mirsaalf@gmail.com>
2024-01-09 20:38:39 -0800

    Added a data migration and updated the database

---
+++ WebsiteProject/Data/Migrations/20240109200324_second.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebsiteProject.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Service",
+                table: "Service");
+
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "Service");
+
+            migrationBuilder.RenameTable(
+                name: "Service",
+                newName: "Services");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Services",
+                newName: "ServicePrice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceDescription",
+                table: "Services",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceName",
+                table: "Services",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Services",
+                table: "Services",
+                column: "ServiceID");
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    ClientID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(25)", maxLength: 25, nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.ClientID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Services",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceDescription",
+                table: "Services");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceName",
+                table: "Services");
+
+            migrationBuilder.RenameTable(
+                name: "Services",
+                newName: "Service");
+
+            migrationBuilder.RenameColumn(
+                name: "ServicePrice",
+                table: "Service",
+                newName: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "Service",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Service",
+                table: "Service",
+                column: "ServiceID");
+        }
+    }
+}
